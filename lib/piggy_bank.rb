7d871b19c0209206b0c15b7080e111e220a6ec21
EC2 Default User <ec2-user@ip-172-31-28-29.eu-west-2.compute.internal>
2020-06-13 14:02:25 +0000

    completed piggy bank program

--- lib/piggy_bank.rb
+++ lib/piggy_bank.rb
@@ -11,10 +11,14 @@ class PiggyBank
     
     def shake
         if @coins > 0
-            "cling" # why if we put puts "cling" it wouldn't work ?
+            "cling" # why if we put puts "cling" it wouldn't work ? the test is to return not to print something on the screen thatere's a different test for puts
         else 
             nil
         end 
     end 
+    def breaks
+        @coins
+    end 
 end 
+
  
--- spec/piggy_bank_spec.rb
+++ spec/piggy_bank_spec.rb
@@ -1,28 +1,49 @@
 require 'piggy_bank'
 describe PiggyBank do
+  
+  let(:piggy_bank) {PiggyBank.new}
+  
   describe 'initialize' do
     it 'can create an instance variable in piggy bank' do
-      piggy_bank = PiggyBank.new 
       expect(piggy_bank).to be_an_instance_of PiggyBank
     end 
-  end 
-  describe'#add_coins' do
-    it 'can add coins to my piggy bank' do 
-      piggy_bank = PiggyBank.new
-      piggy_bank.add_coins(5)
-      expect(piggy_bank.coins).to eq(5)
+    it 'initializes coins to zero' do
+      expect(piggy_bank.coins).to eq(0)
     end 
+  end 
+  describe '#add_coins' do
+    it 'can add coins to my piggy bank' do 
+      piggy_bank.add_coins(5)
+      expect(piggy_bank.coins).to eq(5) 
+    end 
+    it 'does not add coins if there are no coins' do
+      piggy_bank.add_coins(0)
+      expect(piggy_bank.coins).to eq(0)
+    end 
+    it 'adds coins to existing coins in the piggy bank' do
+      piggy_bank.add_coins(7)
+      piggy_bank.add_coins(4)
+      expect(piggy_bank.coins).to eq(11)
+    end
   end 
   describe '#shake' do 
     it 'when piggy bank starts it has no coins in it so it does not cling' do
-      piggy_bank = PiggyBank.new 
-      expect(piggy_bank.coins).to eq(0)
+      expect(piggy_bank.coins).to eq(0) #what would happen if we wrote piggy_bank.add_coins(0) instead
       expect(piggy_bank.shake).to eq(nil)
     end 
     it 'when piggy bank has money in it, it will cling' do 
-      piggy_bank = PiggyBank.new
-      piggy_bank.add_coins(5)
-      expect(piggy_bank.shake).to eq("cling")
+      piggy_bank.add_coins(10)# recommended to use a different number.
+      expect(piggy_bank.shake).to eq("cling")# we only use eq with expect.
     end 
   end 
-end
+  describe '#breaks' do
+    it 'if piggy bank was empty, it will not show coins when it breaks' do 
+      expect(piggy_bank.breaks).to eq(0)
+    end
+    it 'if piggy bank had coins, it will show the number of coins' do
+     piggy_bank.add_coins(3)
+     expect(piggy_bank.breaks).to eq(piggy_bank.coins)
+    end 
+  end
+end 
+    
